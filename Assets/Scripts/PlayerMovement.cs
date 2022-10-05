ce2c2428934c2907c631b074dbdb8a5bac3ff4f4
aelianh <95858325+aelianh@users.noreply.github.com>
2022-10-05 10:15:53 +0200

    cinematica

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -8,12 +9,15 @@ public class PlayerMovement : MonoBehaviour
     private float horizontal;
     private Transform playerTransform;
     private Rigidbody2D rb; 
+    public PlayableDirector director;
+    private Animator anim;
  
     // Start is called before the first frame update
     void Start()
     {
         playerTransform = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     void FixedUpdate() 
     {
@@ -26,10 +30,26 @@ public class PlayerMovement : MonoBehaviour
     {
         
         horizontal = Input.GetAxisRaw("Horizontal");
+        if(horizontal == 0)
+        {
+            anim.SetBool("correr", false);
+        }
+        else
+        {
+            anim.SetBool("correr", true);
+        }
         
         // playerTransform.position += new Vector3 (1, 0, 0)* horizontal * speed * Time.deltaTime;
         //playerTransform.position += new Vector3 (horizontal * speed * Time.deltaTime, 0, 0);
         //playerTransform.Translate(Vector3.right * horizontal * speed * Time.deltaTime, Space.World);
+        
+    }
+    void OnTriggerEnter2D(Collider2D other) 
+    {   
+        if(other.gameObject.tag == "Cinematica")
+        {
+            director.Play(); 
+        }
         
     }
 }   
